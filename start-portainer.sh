227923ecd2d2926e73ed7574202fb0d88b00b5cc
Mark Robert Henderson <mark@mrh.io>
2023-08-25 20:35:53 -0400

    chore: update start-portainer.sh

--- start-portainer.sh
+++ start-portainer.sh
@@ -10,4 +10,4 @@ docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
   -l traefik.http.routers.portainer.rule='Host(`portainer`)' \
   -l traefik.http.routers.portainer.service=portainer \
   -l traefik.http.services.portainer.loadbalancer.server.port=9000 \
-  cr.portainer.io/portainer/portainer-ee:2.16.2-alpine
+  cr.portainer.io/portainer/portainer-ee:2.18.4-alpine
